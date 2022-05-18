6b49324e62bcd34eb1c9959ef4f496a58dc80dc1
Aerox40 <emrekiziltas1981@gmail.com>
2022-05-18 21:58:58 +0300

    Plyer crouch system

--- Assets/Scripts/CrouchScript.cs
+++ Assets/Scripts/CrouchScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CrouchScript : MonoBehaviour
 {
+    CapsuleCollider playerCol;
+    float originalHeight;
+    public float reducedHeight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCol = GetComponent<CapsuleCollider>();
+        originalHeight = playerCol.height;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            Crouch();
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+            GoUp();
         
+    }
+
+    //Method to reduce height
+
+    void Crouch()
+    {
+        playerCol.height = reducedHeight;
+    }
+
+    //Method to reset height
+
+    void GoUp()
+    {
+        playerCol.height = originalHeight;
     }
 }
