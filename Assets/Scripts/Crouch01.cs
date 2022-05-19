130208f883277a9c14f6aad6c0f34fb09f00354e
Alperen Uzun <alperenuzun3d@gmail.com>
2022-05-20 00:48:18 +0300

    Crouch bug fixed

--- Assets/Scripts/Crouch01.cs
+++ Assets/Scripts/Crouch01.cs
@@ -6,16 +6,23 @@ public class Crouch01 : MonoBehaviour
 {
     public CharacterController PlayerHeight;
     public float normalHeight, crouchHeight;
-    
+    private float distance;
+
+    void Start()
+    {
+        distance = normalHeight-crouchHeight;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             PlayerHeight.height = crouchHeight;
         }
-        if (Input.GetKeyUp(KeyCode.LeftControl))
+        if (Input.GetKeyUp(KeyCode.LeftControl) && PlayerHeight.height < normalHeight)
         {
             PlayerHeight.height = normalHeight;
+            transform.position = new Vector3(transform.position.x, transform.position.y + (distance / 2), transform.position.z);
         }
     }
 }
