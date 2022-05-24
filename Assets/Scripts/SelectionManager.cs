775e744d4b6cb15af50cd29712ebb3e6bee35895
Alperen Uzun <alperenuzun3d@gmail.com>
2022-05-24 23:50:28 +0300

    Key picking door lock working

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -4,11 +4,12 @@ public class SelectionManager : MonoBehaviour
 {
     public float interactionDistance; // 1 works fine.
 
-    private bool openDoor;
+    public static int openDoor;
+    private bool gotKey;
     private int limit1;
     private int limit2;
 
-    private bool redOpedDoor;
+    private int redOpedDoor;
 
     public float mainDoorSpeed;
     public float cabinetDoorsSpeed;
@@ -32,39 +33,46 @@ public class SelectionManager : MonoBehaviour
                 var selectionRenderer = selection.GetComponent<Renderer>();
                 if (selectionRenderer != null)
                 {
-                    //print(selectionRenderer.name);
+                    print(selectionRenderer.name);
 
-                    if (selectionRenderer.name == "doorWing")
+                    if (selectionRenderer.name == "doorWing" && openDoor == 1)
                     {
-                        openDoor = true;
+                        openDoor = 2;
                     }
+                    
 
-                    if (selectionRenderer.name == "DoorLeft" || selectionRenderer.name == "DoorRight")
+                    if (selectionRenderer.name == "DoorLeft" || selectionRenderer.name == "DoorRight" && redOpedDoor == 0)
                     {
                         //print("HORSE");
-                        redOpedDoor = true;
+                        redOpedDoor = 1;
                         GameObject.Find("DoorLeft").GetComponent<BoxCollider>().isTrigger = true;
                         GameObject.Find("DoorRight").GetComponent<BoxCollider>().isTrigger = true;
+                    }
+
+                    if (selectionRenderer.name == "sm_key_01" && openDoor == 0)
+                    {
+                        selection.gameObject.SetActive(false);
+                        openDoor = 1;
                     }
                 }
             }
         }
 
-        if (openDoor)
+        if (openDoor == 2)
         {
             GameObject pv = GameObject.Find("doorWing");
             GameObject.Find("doorWing").transform.rotation = Quaternion.Euler(new Vector3(pv.transform.rotation.eulerAngles.x, pv.transform.rotation.eulerAngles.y + (mainDoorSpeed * 0.5f), pv.transform.rotation.eulerAngles.z));
             limit1++;
         }
 
-        if (limit1 > 200/mainDoorSpeed && openDoor)
+        if (limit1 > 200/mainDoorSpeed && openDoor == 2)
         {
-            openDoor = false;
+            openDoor = 3;
             GameObject.Find("doorWing").GetComponent<BoxCollider>().isTrigger = true;
             limit1 = 0;
         }
 
-        if (redOpedDoor)
+        if (redOpedDoor == 1)
         {
             GameObject pv = GameObject.Find("DoorLeft");
             GameObject.Find("DoorLeft").transform.rotation = Quaternion.Euler(new Vector3(pv.transform.rotation.eulerAngles.x, pv.transform.rotation.eulerAngles.y + (cabinetDoorsSpeed * 0.1f), pv.transform.rotation.eulerAngles.z));
@@ -73,9 +81,9 @@ public class SelectionManager : MonoBehaviour
             limit2++;
         }
 
-        if (limit2 > 800/cabinetDoorsSpeed && redOpedDoor)
+        if (limit2 > 800/cabinetDoorsSpeed && redOpedDoor == 1)
         {
-            redOpedDoor = false;
+            redOpedDoor = 2;
             limit2 = 0;
         }
     }
