6f5ba72b7c592179de599e4bda7ee5308f2209eb
Alperen Uzun <alperenuzun3d@gmail.com>
2022-06-09 22:08:55 +0300

    Manual Time Stop

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -10,6 +10,8 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject panel1, panel2, panel3, panel4, panel5;
 
+    public GameObject playerCapsule;
+
     // Update is called once per frame
     void Update()
     {
@@ -23,32 +25,54 @@ public class PauseMenu : MonoBehaviour
                 Pause();
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StopMovement();
+        }
     }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+        //Time.timeScale = 1f;
+        StartMovement();
         GameIsPaused = false;
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        //Time.timeScale = 0f;
+        StopMovement();
         GameIsPaused = true;
     }
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Time.timeScale = 0f;
+        //Cursor.lockState = CursorLockMode.None;
+        //Time.timeScale = 0f;
+        StopMovement();
     }
 
     public void Panel1Close()
     {
         panel1.SetActive(false);
-        Time.timeScale = 1f;
-        
+        //Time.timeScale = 1f;
+        StartMovement();
+    }
+
+    private void StopMovement()
+    {
+        playerCapsule.GetComponent<CharacterController>().gameObject.SetActive(false);
+        playerCapsule.GetComponent<Crouch01>().gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void StartMovement()
+    {
+        playerCapsule.GetComponent<CharacterController>().gameObject.SetActive(true);
+        playerCapsule.GetComponent<Crouch01>().gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
